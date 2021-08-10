6a9cd116e159a61db56b51ab0304045f3405c187
lukehayes <lukepurplehayes@gmail.com>
2021-08-10 17:51:52 +0100

    Updated comment style of Database class.

--- app/database.py
+++ app/database.py
@@ -4,13 +4,19 @@ import pprint
 class Database(object):
     """
     Database class that connects to an sqlite3 instance.
-
-    :param str name: The name of the database - defaults to 'db.sqlite'
-
     """
+
     def __init__(self, db_name = 'db.sqlite'):
+        """
+        Constructor.
+
+        :param str name: The name of the database - defaults to 'db.sqlite'
+        """
         self._connection = sqlite3.connect(db_name, check_same_thread=False)
+
+        # Return results as objects
         self._connection.row_factory = sqlite3.Row
+
         self.cursor = self._connection.cursor()
 
     def close(self):
@@ -18,6 +24,11 @@ class Database(object):
         self._connection.close()
 
     def all(self, table_name):
+        """
+        Run a select * on a specific table.
+
+        :param str table_name: The name of the table
+        """
         queryString = "select * from " + table_name
         query = self.cursor.execute(queryString)
         return query.fetchall()
