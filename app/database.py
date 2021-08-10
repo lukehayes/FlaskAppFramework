20b806d388b4a2824a2edc83e66986b3fe351e6e
lukehayes <lukepurplehayes@gmail.com>
2021-08-10 16:30:47 +0100

    Added Database.close() method and renamed database name to 'db.sqlite',

--- app/database.py
+++ app/database.py
@@ -4,8 +4,12 @@ class Database(object):
     """
     Database class that connects to an sqlite3 instance.
 
-    :param str name: The name of the database - defaults to 'database.db'
+    :param str name: The name of the database - defaults to 'db.sqlite'
 
     """
-    def __init__(self, db_name = "database.db"):
+    def __init__(self, db_name = 'db.sqlite'):
         self._connection = sqlite3.connect(db_name)
+
+    def close(self):
+        """Close the connection to the database."""
+        self._connection.close()
