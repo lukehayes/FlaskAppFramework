67d7e47677d11b8b081fc78d3a0d883a73be147f
lukehayes <lukepurplehayes@gmail.com>
2021-08-10 18:12:40 +0100

    main.py now uses an instance of Database().

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from flask import Flask, url_for, request, render_template
+from app.database import Database
+import pprint
 
 app = Flask(__name__)
 
+db = Database()
+
 @app.route("/")
 def index():
-    return render_template("hello.html")
+    data = db.all("data")
+    return render_template("hello.html", d = data)
+
+@app.route("/about")
+def about():
+    return "About"
 
 # with app.test_request_context():
     # print("Loaded")
