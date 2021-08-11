cee12458d7b54f254594769e328612eb62f3e6f0
lukehayes <lukepurplehayes@gmail.com>
2021-08-11 09:44:03 +0100

    Added schema.sql for ease of use.

---
+++ app/schema.sql
@@ -0,0 +1,7 @@
+DROP TABLE data;
+
+CREATE TABLE data(id integer, name text, number integer);
+
+INSERT INTO data(id, name, number) VALUES(1, "Bob", 321);
+INSERT INTO data(id, name, number) VALUES(2, "John", 1002);
+INSERT INTO data(id, name, number) VALUES(3, "Armando", 564);
