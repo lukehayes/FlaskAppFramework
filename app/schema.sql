3c550c50accc2d560af7ee022a0ea6d665827366
lukehayes <lukepurplehayes@gmail.com>
2021-08-11 12:51:16 +0100

    Data insert sql now uses multiline syntax.

--- app/schema.sql
+++ app/schema.sql
@@ -1,13 +1,18 @@
 DROP TABLE if exists data;
 DROP TABLE if exists users;
 
+-- Data Table
 CREATE TABLE data(id integer, name text, number integer);
 
-INSERT INTO data(id, name, number) VALUES(1, "Bob", 321);
-INSERT INTO data(id, name, number) VALUES(2, "John", 1002);
-INSERT INTO data(id, name, number) VALUES(3, "Armando", 564);
+INSERT INTO data(id, name, number) VALUES
+    (1, "Bob", 321),
+    (2, "John", 1002),
+    (3, "Armando", 564);
 
+
+-- Users Table
 CREATE TABLE users(id integer, username text, email integer, password text);
+
 INSERT INTO users(id, username, email, password) VALUES
     (1, "developer", "developer@app.com", "password"),
     (2, "admin", "admin@app.com", "password"),
