678bd490fd49ea4c0f09f09889ac937a7c6f09bb
lukehayes <lukepurplehayes@gmail.com>
2021-08-16 09:37:42 +0100

    Added start of login inside app/auth.py

--- app/auth.py
+++ app/auth.py
@@ -12,4 +12,19 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/register', methods = ['GET', 'POST'])
 def register():
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = Database()
+        error = None
+
+        if not username:
+            error = 'The username is missing.'
+        if not password:
+            error = 'The password is missing.'
+
+        if error is None:
+            print("Running database queries...")
+
     return render_template('auth/register.html')
