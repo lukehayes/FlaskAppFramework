1b9fef25bd7f882d7ebfc7ec6ce441540916564c
lukehayes <lukepurplehayes@gmail.com>
2021-08-18 09:55:30 +0100

    Removed sql for data insert etc.

--- app/schema.sql
+++ app/schema.sql
@@ -1,14 +1,4 @@
-DROP TABLE if exists data;
 DROP TABLE if exists users;
-
--- Data Table
-CREATE TABLE data(id integer, name text, number integer);
-
-INSERT INTO data(id, name, number) VALUES
-    (1, "Bob", 321),
-    (2, "John", 1002),
-    (3, "Armando", 564);
-
 
 -- Users Table
 CREATE TABLE users(id integer, username text, email integer, password text);
