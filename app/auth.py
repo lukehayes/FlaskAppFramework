5642016e844ea27210daa8eec72d4689865c3bd0
lukehayes <lukepurplehayes@gmail.com>
2021-08-18 10:16:20 +0100

    Starting to add basic login code to /login route.

--- app/auth.py
+++ app/auth.py
@@ -32,4 +32,31 @@ def register():
 
 @bp.route('/login', methods = ['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        db = Database()
+        error = None
+
+        print(generate_password_hash("password"))
+
+        query = 'SELECT * FROM users WHERE username = "{}"'.format(username)
+        print(query)
+
+        user = db.raw_query(
+            'SELECT * FROM users WHERE username = "developer"')
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
     return render_template('auth/login.html')
+
