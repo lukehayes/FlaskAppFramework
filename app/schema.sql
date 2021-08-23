7b5f4f623b52d70ec7c70179698231866f1ec520
lukehayes <lukepurplehayes@gmail.com>
2021-08-23 10:49:02 +0100

    Schema.sql now only inserts a single row.

--- app/schema.sql
+++ app/schema.sql
@@ -4,7 +4,7 @@ DROP TABLE if exists users;
 CREATE TABLE users(id integer PRIMARY KEY, username text, email integer, password text);
 
 INSERT INTO users(id, username, email, password) VALUES
-    (1, "developer", "developer@app.com", "pbkdf2:sha256:260000$FfYLQKXio9dkssIi$57a00682fe12f78fe040f2ff7afe23201331328d6be54b30ec5468826a0fafce"),
-    (2, "admin", "admin@app.com", "admin"),
-    (3, "testuser", "testuser@app.com", "password");
+    (1, "developer", "developer@app.com", "pbkdf2:sha256:260000$FfYLQKXio9dkssIi$57a00682fe12f78fe040f2ff7afe23201331328d6be54b30ec5468826a0fafce");
+    -- (2, "admin", "admin@app.com", "admin"),
+    -- (3, "testuser", "testuser@app.com", "password");
 
